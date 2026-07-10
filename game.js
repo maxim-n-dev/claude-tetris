d215cd2004493c45dfe8cecf9378d9caa0f5bc04
Maxim Nakonechnyy <maxim.n.dev@gmail.com>
2026-07-10 20:35:37 +0200

    update game.js to add a new piece 3x3 with a hole in the middle

--- game.js
+++ game.js
@@ -13,6 +13,7 @@ const COLORS = [
   '#e57373', // Z - red
   '#64b5f6', // J - pale blue
   '#ffb74d', // L - orange
+  '#b0bec5', // Nut - steel gray
 ];
 
 const PIECES = [
@@ -24,6 +25,7 @@ const PIECES = [
   [[5,5,0],[0,5,5],[0,0,0]],                  // Z
   [[6,0,0],[6,6,6],[0,0,0]],                  // J
   [[0,0,7],[7,7,7],[0,0,0]],                  // L
+  [[8,8,8],[8,0,8],[8,8,8]],                  // Nut (tuerca) - agujero central
 ];
 
 const LINE_SCORES = [0, 100, 300, 500, 800];
@@ -73,7 +75,8 @@ function createBoard() {
 }
 
 function randomPiece() {
-  const type = Math.floor(Math.random() * 7) + 1;
+  const NUT_CHANCE = 0.12; // ~1 de cada ~8 piezas es la tuerca
+  const type = Math.random() < NUT_CHANCE ? 8 : Math.floor(Math.random() * 7) + 1;
   const shape = PIECES[type].map(row => [...row]);
   return { type, shape, x: Math.floor(COLS / 2) - Math.floor(shape[0].length / 2), y: 0 };
 }
